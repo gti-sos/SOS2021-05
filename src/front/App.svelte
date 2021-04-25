ed2c88eaf53a21655e40ab3a1fb254eb899fd6ba
manu261998 <manu261998@gmail.com>
2021-04-25 21:50:42 +0200

    por dios que se despliegue

--- src/front/App.svelte
+++ src/front/App.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Router from 'svelte-spa-router';
-	import Sales from "src/front/arms-sales-stats/Arms-sales-stats.svelte";
-	import Homicides from "src/front/homicides/Homicides.svelte";
+	import Sales from "./arms-sales-stats/Arms-sales-stats.svelte";
+	import Homicides from "./homicides/Homicides.svelte";
 	
 	import Info from "./Info.svelte";
 	const routes = {
