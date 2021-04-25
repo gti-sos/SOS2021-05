bf6be7fc4d9a2d408f2c58fecfd27eee24a964b4
manu261998 <manu261998@gmail.com>
2021-04-25 14:59:06 +0200

    tabla ok

--- src/front/App.svelte
+++ src/front/App.svelte
@@ -1,10 +1,30 @@
 <script>
+	import Table_svelte from './homicides/Homicides.svelte'
 	export let name;
+	
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	 <input type="button" id="b1" value="Games" onClick="document.getElementById('oculto').style.visibility='visible'">
+	 <input type="button" value="Ocultar" onClick="document.getElementById('oculto').style.visibility='hidden'">
+	<br>
+   
+    
+    <div id="oculto" style="visibility:visible">
+       
+		<Table_svelte/>
+
+    </div>
+<style type="text/css">
+  table, th, td {
+  border: 1px solid black;
+  text-align:center;
+  margin: auto;
+}
+
+  </style>
 </main>
 
 <style>
@@ -21,6 +41,8 @@
 		font-size: 4em;
 		font-weight: 100;
 	}
+
+	
 
 	@media (min-width: 640px) {
 		main {
---
+++ src/front/homicides/homicides.svelte
@@ -0,0 +1,58 @@
+<script>
+    import {
+        onMount
+    } from "svelte";
+
+    
+    let data = [];
+    async function getGames(){
+        
+        console.log("Fetching data ...");
+        const res = await fetch("./api/v2/homicides-by-firearms");
+        
+        if(res.ok){
+            console.log("Ok.");
+            const json = await res.json();
+            data = json;
+            
+        }else{
+            console.log("Error!");
+        }
+    }
+    onMount(getGames);
+</script>
+
+<main>
+    <table >
+        <thead>
+            <tr>
+                <td>State</td>
+                <td>Year</td>
+                <td>Homicides by firearms</td>
+                <td>Handgun</td>
+                <td>Rifle</td>
+                <td>Shotgun</td>
+                <td>Type not stated</td>
+
+            </tr>
+        </thead>
+        <tbody>
+            {#each data as data}
+                <tr>
+                    <td>{data.state}</td>
+                    <td>{data.year}</td>
+                    <td>{data.homicide_by_firearm}</td>
+                    <td>{data.handgun}</td>
+                    <td>{data.rifle}</td>
+                    <td>{data.shotgun}</td>
+                    <td>{data.type_not_stated}</td>
+
+
+                </tr>
+            {/each}
+        </tbody>
+    </table >
+
+    
+
+</main>
