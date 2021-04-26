1ee1b87f4a671f92094fde151a53656e6026d11d
josanmegias <josmegmac@alum.us.es>
2021-04-26 23:07:53 +0000

    .svelte actualizado

--- src/front/attacks-stats/attacks-stats.svelte
+++ src/front/attacks-stats/attacks-stats.svelte
@@ -1,31 +1,65 @@
-<script>
-    import {
-        onMount
-    } from "svelte";
-
- 
- 
+<script lang="ts">
+   
+import {Button, Table, Toast, ToastBody, ToastHeader } from 'sveltestrap';
+    const BASE_API_URL = "/api/v2/attacks-stats"; //tiene que llamar a la API para tratar los datos
 	
+    let cargados = false;
     let data = [];
-    async function getGames(){
-        
-        console.log("Fetching data ...");
-        const res = await fetch("./api/v2/attacks-stats");
-        
-        if(res.ok){
+    async function loadStats(){
+        console.log("Loading data...");
+        const carga =  await fetch(BASE_API_URL + "/loadInitialData");
+        cargados = true;
+        if (carga.ok){
             console.log("Ok.");
-            const json = await res.json();
-            data = json;
-            
+            const res = await fetch(BASE_API_URL);
+            if(res.ok){
+                console.log("Ok. Obtaining data...")
+                const json = await res.json();
+                data = json;
+                console.log('Received ${data.length} life stats.');
+            }else{
+                console.log("Error, there is no data.")
+            }
         }else{
-            console.log("Error!");
+            console.log("Error loading data.");
         }
     }
-    onMount(getGames);
+   
+    async function deleteStats() {
+		console.log("Deleting life stats...");
+        cargados=false;
+		const res = await fetch(BASE_API_URL, {
+			method: "DELETE"
+		}).then(function (res) {
+			if (res.ok){
+				console.log("Ok.");
+                data = [];
+			} else if (res.status==404){ //no data found
+                console.log("No data found");
+			} else  { 
+				console.log("Error deleting DB stats");
+			}
+			
+		});
+	}
 </script>
+  
 
 <main>
-    <table>
+    <div>
+        {#if cargados}  
+        <Button style="background-color: crimson;" disabled> Cargar datos</Button>
+        {:else}
+        <Button style="background-color: crimson;" on:click={loadStats}> Cargar datos</Button>
+        {/if}
+        <Button style="background-color: darkgray" on:click={deleteStats}> Eliminar datos</Button>
+        
+    </div>
+    
+  
+    {#if data.length != 0}
+        <br/>
+        <table>
         <thead>
             <tr>
                 <td><b>State</b></td>
@@ -61,15 +95,26 @@
             {/each}
         </tbody>
     </table>
-
-    
+        <a href="/">Volver</a>
+    {:else}
+    <br/>
+    <p style="text-align: center; background-color: antiquewhite;"> Para ver los datos pulse el botón.</p>
+    <a href="/">Volver</a>
+    {/if}
 
 </main>
-<style>
 
-    table, tr, td {
-      border: 1px solid black;
-      
+
+<style>
+    a {
+        font-size: 18px;
+        background-color:rgb(103, 131, 72);
+        color: white;
+        border-radius: 6px;
+        border: 1px solid grey;
+        padding:4px;
     }
-    
-    </style>
+    a:hover {
+        color:white;
+    }
+</style>
