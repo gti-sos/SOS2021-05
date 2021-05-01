6b9db1df8b622b08602ae84e2c1f91f3d871005d
Iván <ivanmartin2h@gmail.com>
2021-05-01 09:41:50 +0200

    Front-End homicidios añadidos

--- src/front/homicides/homicides.svelte
+++ src/front/homicides/homicides.svelte
@@ -1,191 +1,313 @@
 <script lang="ts">
    
-    import {Button, Table, Toast, ToastBody, ToastHeader } from 'sveltestrap';
-        const BASE_API_URL = "/api/v2/homicides-by-firearms"; //tiene que llamar a la API para tratar los datos
-        
-        let cargados = false;
-        let data = [];
-        let newData = {
-            state:"",
-            year:"",
-            homicide_by_firearm: "" ,
-            handgun:"",
-            rifle:"",
-            shotgun:"",
-            type_not_stated: ""
-
-        }
-
-        //funcion asincrona para cargar los datos iniciales (loadInitialData)
-        async function loadStats(){
-            deleteStats();
-            console.log("Loading data...");
-            const carga =  await fetch(BASE_API_URL + "/loadInitialData");
-            cargados = true;
-            if (carga.ok){
-                console.log("Ok.");
-                const res = await fetch(BASE_API_URL);
-                if(res.ok){
-                    console.log("Ok. Obtaining data...")
-                    const json = await res.json();
-                    data = json;
-                    console.log('Received ${data.length} life stats.');
-                }else{
-                    console.log("Error, there is no data.")
-                }
-            }else{
-                console.log("Error loading data.");
-            }
-        }
-       
-        //funcion asincrona para cargar (get) los recursos existentes
-        async function getData() {
-            console.log("Fetching homicides resourcers...");
-            const res = await fetch(BASE_API_URL);
-            if(res.ok){
-                const json = await res.json();
-                data = json;
-                console.log(`Received ${data.length} resources`);
-
-            }else{
-                console.log("ERROR!");
-            }
-
-        }
-
-
-       
-
-        //funcion asincrona para borrar todos los recursos
-        async function deleteStats() {
-            console.log("Deleting life stats...");
-            cargados=false;
-            const res = await fetch(BASE_API_URL, {
-                method: "DELETE"
-            }).then(function (res) {
-                if (res.ok){
-                    console.log("Ok.");
-                    data = [];
-                } else if (res.status==404){ //no data found
-                    console.log("No data found");
-                } else  { 
-                    console.log("Error deleting DB stats");
-                }
-                
-            });
-        }
-        
-        
-        async function insertData() { //insertar un recurso en concreto
-            console.log("Inserting new resource " + JSON.stringify(newData));
-            const res = await fetch(BASE_API_URL, {
-                method: "POST",
-                body: JSON.stringify(newData),
-                headers: {
-                    "Content-Type": "application/json",
-                }
-            }
-            ).then( (res) => {
-                getData();
-            })
-            
-        }
-
-
-        async function deleteData() { //elimina un recurso en concreto
-            console.log("Deleting resource " + JSON.stringify(data));
-            const res = await fetch(BASE_API_URL, {
-                method: "DELETE",
-                body: JSON.stringify(newData),
-            }
-            ).then( (res) => {
-                getData();
-            })
-            
-        }
-
-
-        
-        
-       
-
-    </script>
-      
+    import {
+         Nav,
+         Modal,
+         ModalBody,
+         ModalFooter,
+         ModalHeader,
+         NavItem,
+         NavLink,
+         Button,
+         Table,
+         UncontrolledAlert,
+         Card,
+         CardBody,
+         CardFooter,
+         CardHeader,
+         CardSubtitle,
+         CardText,
+         CardTitle,
+     } from "sveltestrap";
+     
+     import {
+        pop
+     } from "svelte-spa-router";
+ 
+     
+     const BASE_API_URL = "/api/v2/homicides-by-firearms"; //tiene que llamar a la API para tratar los datos
+     
+     let cargados = false;
+     let data = [];
+ 
+     let isOpen = false;
+ 
+     getData();
+ 
+     let newData = {
+             state:"",
+             year:"",
+             homicide_by_firearm: "" ,
+             handgun:"",
+             rifle:"",
+             shotgun:"",
+             type_not_stated: ""
+         }
+ 
+      //funcion asincrona para cargar (get) los recursos existentes
+      async function getData() {
+             console.log("Fetching homicides resourcers...");
+             const res = await fetch(BASE_API_URL);
+             if(res.ok){
+                 const json = await res.json();
+                 data = json;
+                 console.log(`Received ${data.length} resources`);
+ 
+             }else{
+                 console.log("ERROR!");
+             }
+ 
+         }
+ 
+         async function insertData() { //insertar un recurso en concreto
+             console.log("Inserting new resource " + JSON.stringify(newData));
+             
+             const res = await fetch(BASE_API_URL, {
+                 method: "POST",
+                 body: JSON.stringify(newData),
+                 headers: {
+                     "Content-Type": "application/json",
+                 }
+             }
+             ).then( (res) => {
+                 getData();
+             })
+             
+         }
+ 
+ 
+ 
+         async function deleteData( a, b) { //elimina un recurso en concreto
+             
+             console.log("Deleting resource " + JSON.stringify(data));
+             const res = await fetch(BASE_API_URL+"/"+a+"/"+b+"/", {
+                 method: "DELETE",
+               
+             })
+             getData();
+         }
+ 
+         async function loadStats(){
+         deleteStats();
+         console.log("Loading data...");
+         const carga =  await fetch(BASE_API_URL + "/loadInitialData");
+         cargados = true;
+         if (carga.ok){
+             console.log("Ok.");
+             const res = await fetch(BASE_API_URL);
+             if(res.ok){
+                 console.log("Ok. Obtaining data...")
+                 const json = await res.json();
+                 data = json;
+                 console.log('Received ${data.length} life stats.');
+             }else{
+                 console.log("Error, there is no data.")
+             }
+         }else{
+             console.log("Error loading data.");
+         }
+     }
     
-    <main>
-        <div>
-            {#if cargados}   <!--  Si ya hemos cargado los datos iniciales--> 
-            <Button style="background-color: crimson;" disabled> Cargar datos Iniciales</Button>
-            <Button style="background-color: crimson;" on:click={insertData}> Insertar Recurso</Button>
-            <td>Año<input bind:value = "{newData.state}"></td>
-            <td>Estado<input bind:value = "{newData.year}"></td>
-            <td>Homicidios por armas<input bind:value = "{newData.homicide_by_firearm}"></td>
-            <td>Armas de mano<input bind:value = "{newData.handgun}"></td>
-            <td>Rifle<input bind:value = "{newData.rifle}"></td>
-            <td>Tipo no especificado<input bind:value = "{newData.shotgun}"></td>
-
-            {:else}
-    
-            <Button style="background-color: crimson;" on:click={loadStats}> Cargar Datos Iniciales</Button>
-            {/if}
-            <Button style="background-color: darkgray" on:click={deleteStats}> Eliminar datos</Button>
-
-            
-        </div>
+     async function deleteStats() {
+         console.log("Deleting life stats...");
+         cargados=false;
+         const res = await fetch(BASE_API_URL, {
+             method: "DELETE"
+         }).then(function (res) {
+             if (res.ok){
+                 console.log("Ok.");
+                 data = [];
+             } else if (res.status==404){ //no data found
+                 console.log("No data found");
+             } else  { 
+                 console.log("Error deleting DB stats");
+             }
+             
+         });
+     }
+ 
+      //Insert
+     let open1 = false;
+     const toggle1 = () => (open1 = !open1);
+     const toggle1P = () => {
+         open1 = !open1;
+         console.log("Imprimo: "+newData.state.length)
+         if(newData.state.replace(' ', '').length!=0 
+         &&newData.year.replace(' ', '').length!=0 
+         && newData.homicide_by_firearm.length!=0 
+         && newData.handgun.length!=0 
+         && newData.rifle.length!=0 
+         && newData.shotgun.length!=0
+         && newData.type_not_stated.length!=0){
+ 
+         insertData()
+         getData();
+         }
+         else{
+ 
+             console.log("Nada añadido")
+             popinsert=true;
+         }
+ 
+     };
+ 
+     let popinsert = false;
+     const togglepop = () => (popinsert = !popinsert);
+     const togglepopok = () => {
+         popinsert = !popinsert;
+         open1=true}
+     
+     
+ 
+ </script>
+   
+ 
+ <!-- svelte-ignore missing-declaration -->
+ <main>
+     <div>
+         {#if cargados}  
+         <Button style="background-color: #F39C12;" disabled> Cargar datos</Button>
+         {:else}
+         <Button style="background-color: #F39C12;" on:click={loadStats}> Cargar datos</Button>
+         {/if}
+         <Button style="background-color: #F08080" on:click={deleteStats}> Eliminar datos</Button>
+         <Button style="background-color: #28B463" on:click={toggle1}> Insertar</Button>
+ 
         
-      
-        {#if data.length != 0}
-            <br/>
-            <table >
-            <thead>
-                <tr>
-                    <td>State</td>
-                    <td>Year</td>
-                    <td>Homicide by firearm</td>
-                    <td>Handgun</td>
-                    <td>Rifle</td>
-                    <td>Shotgun</td>
+             <div id="modal">
+             <Modal isOpen={open1} toggle={toggle1} transitionOptions>
+                 <ModalHeader {toggle1}>¿Quieres insertar un nuevo dato?</ModalHeader>
+                 <ModalBody >
+                     Por favor, rellene el formulario. Todos los campos tienen que tener un valor. De lo contrario no se añadirá nada.
+                     <tr>
+                         <Table >
+                             
+                             <tbody>
+                                 
+                                    
+                                     <tr>
+                                         <td>Estado</td>
+                                         <td><input bind:value="{newData.state}"></td>
+                                         
+                                         
+                                     </tr><tr>
+                                         <td>Año</td>
+                                         <td><input bind:value="{newData.year}"> </td>
+                                    
+                                         
+                                     </tr><tr>
+                                         <td>Homicidios por armas</td>
+                                         <td><input bind:value="{newData.homicide_by_firearm}"> </td>
+                                        
+                                     </tr><tr>
+                                         <td>Armas de mano</td>
+                                         <td><input bind:value="{newData.handgun}"> </td>
+                                         
+                                         
+                                     </tr><tr>
+                                         <td>Rifles (armas largas)</td>
+                                         <td><input bind:value="{newData.rifle}"> </td>
+                                    </tr><tr>
+                                        <td>Escopetas</td>
+                                        <td><input bind:value="{newData.shotgun}"> </td>
 
+                                    </tr><tr>
+                                        <td>Tipo no indicado</td>
+                                        <td><input bind:value="{newData.type_not_stated}"> </td>
+                                        
+                                            
+                                         
+                                     </tr>
+ 
+ 
+ 
+                                
+                             </tbody>
+                         </Table >
+                     </tr>
+                 </ModalBody>
+                 <ModalFooter>
+                     <Button color="primary" on:click={toggle1P}>Insertar</Button>
+                     <Button color="secondary" on:click={toggle1}
+                         >Cancelar</Button
+                     >
+                 </ModalFooter>
+             </Modal>
+ 
+ 
+             <Modal isOpen={popinsert} toggle={togglepop} transitionOptions>
+                 <ModalHeader {togglepop}>Se ha producido un error</ModalHeader>
+                 <ModalBody >
+                     No se ha podido insertar el dato. El Nombre, Año o Mes no tiene un formato correcto o hay un campo vacio.
                     
-                </tr>
-            </thead>
-            <tbody>
-                {#each data as data}
-                    <tr>
-                        <td>{data.state}</td>
-                        <td>{data.year}</td>
-                        <td>{data.homicide_by_firearm}</td>
-                        <td>{data.handgun}</td>
-                        <td>{data.rifle}</td>
-                        <td>{data.shotgun}</td>
+                 </ModalBody>
+                 <ModalFooter>
+                     <Button color="primary" on:click={togglepopok}>Probaré de nuevo!</Button>
+                     <Button color="secondary" on:click={togglepop}>Cancelar</Button>
+                 </ModalFooter>
+             </Modal>
+         </div>
+ 
+ 
+     </div>
+     
+   
+     {#if data.length != 0}
+         <br/>
+         <Table bordered  style="background-color: #F5EEF8 ; width:75% ; text-align: center; ">
+         <thead style="background-color: #E8DAEF; color:black">
+             <tr>
+                 <td>Estado</td>
+                 <td>Año</td>
+                 <td>Homicidios por armas</td>
+                 <td>Armas de mano</td>
+                 <td>Rifles (armas largas)</td>
+                 <td>Escopetas</td>
+                 <td>Tipo no indicado</td>
+             </tr>
+         </thead>
+         <tbody>
+             {#each data as data}
+                 <tr>
+                     <td><a href="/data.state/data.year">{data.state}</a></td>
+                     <td>{data.year}</td>
+                     <td>{data.homicide_by_firearm}</td>
+                     <td>{data.handgun}</td>
+                     <td>{data.rifle}</td>
+                     <td>{data.shotgun}</td>
+                     <td>{data.type_not_stated}</td>
 
-                      
+                     <td>
+                         <Button style="background-color: #F08080" on:click={() =>deleteData(data.state,data.year)}> Eliminar</Button>
+                         <Button style="background-color: #28B463" on:click={deleteStats}> Actualizar</Button>
+                     </td>
+                   
+ 
+ 
+ 
+                 </tr>
+             {/each}
+         </tbody>
+     </Table >
+         <Button color="dark" on:click={pop}>Volver</Button>
+     {:else}
+     <br/>
+     <p style="text-align: center; background-color: antiquewhite;"> Para ver los datos pulse el botón.</p>
+     
+         <Button color="dark" on:click={pop}>Volver</Button>
+     {/if}
+ 
+ </main>
+ 
+ 
+ <style>
     
+     a:hover {
+         color:white;
+     }
+ 
+   
     
-                    </tr>
-                {/each}
-            </tbody>
-        </table >
-            <a href="/">Volver</a>
-        {:else}
-        <br/>
-        <p style="text-align: center; background-color: antiquewhite;"> Para ver los datos pulse el botón.</p>
-        <a href="/#/info">Volver</a>
-        {/if}
-    
-    </main>
-    
-    
-    <style>
-        a {
-            font-size: 18px;
-            background-color:rgb(103, 131, 72);
-            color: white;
-            border-radius: 6px;
-            border: 1px solid grey;
-            padding:4px;
-        }
-        a:hover {
-            color:white;
-        }
-    </style>
+ 
+     
+ </style>
