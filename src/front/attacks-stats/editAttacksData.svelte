1f4be496b722722b5c3011e2f3c8fd581262b86b
josanmegias <josepantoniomegias@hotmail.com>
2021-05-08 16:57:59 +0000

    Svelte editAttacks actualizado

--- src/front/attacks-stats/editAttacksData.svelte
+++ src/front/attacks-stats/editAttacksData.svelte
@@ -22,7 +22,9 @@
     import {pop} from "svelte-spa-router";
     export let params = {};
     
-    let noencontrado = false;
+    
+   onMount(getCrime);
+    
 
     let sale = {};
     let state;
@@ -37,15 +39,14 @@
     let type_of_attack_gun;
     let type_of_attack_knife;
     
-    let userMsg;
-    onMount(getCrime);
-    
+
 
     async function getCrime(){
         console.log("Buscando crimen...");
         const res = await fetch("/api/v2/attacks-stats/"+params.state+"/"+params.year);
         
-        if (res.status==200){
+        switch(res.status){
+        case 200:
             console.log("OK!");
             const json= await res.json();
             sale = json ;
@@ -60,21 +61,36 @@
 	    type_of_attack_personal_weapons = sale.type_of_attack_personal_weapons;
 	    type_of_attack_gun = sale.type_of_attack_gun;
             type_of_attack_knife = sale.type_of_attack_knife;
+
+            let mensajeaux= "El dato: "+params.state+"/"+params.year+" se encuentra en la Base de Datos"    
+            lanzamensaje(res.status,res.statusText,"Se ha encontrado el recurso buscado",mensajeaux,null)
+            
            
             console.log("Crimen recibido.");
-        }else{
-            noencontrado = true;
+        break;
+
+        case 404:
+            let mensajeaux2= "El dato: "+params.state+"/"+params.year+" no se ha encontrado en la Base de datos"    
+            lanzamensaje(res.status,res.statusText,"Se ha producido un error al buscar el elemento",mensajeaux2,true)
             console.log("Error, algo ha ido mal");
+        break;
+
+        default:
+            lanzamensaje(res.status,res.statusText,"Se ha producido un error al buscar el elemento","Vaya... Algo ha salido mal. Probablemente la Base de Datos haya tenido un problema. Vuelva a intentarlo mas adelante",true)
+        break;
         }
+       
+
+
     }
     
     async function updateCrime(){
         console.log('Actualizando crimen con '+ JSON.stringify(params.state)+" "+JSON.stringify(params.year));
-		const res = await fetch("/api/v2/attacks-stats/"+params.state+"/"+params.year,{
+		const res = await fetch("/api/v2/attacks-stats/"+state+"/"+year,{
 			method: "PUT",
 			body: JSON.stringify({
-                state: params.state,
-                year: params.year,
+                state: state,
+                year: year,
                 sex_male: sex_male,
 	    	sex_female: sex_female,
 	    	sex_unknown: sex_unknown,
@@ -85,14 +101,44 @@
 	    	type_of_attack_gun: type_of_attack_gun,
             	type_of_attack_knife: type_of_attack_knife
                 
+				
             }),
 			headers: {
 				"Content-Type": "application/json"
 			}
 		}).then(function(res){
-            
-            userMsg = "DATO ACTUALIZADO";
+           
+            switch(res.status){
+
+            case 200:
+                botontomain=true;
+                let mensajeaux= "El dato: "+params.state+"/"+params.year+" ha sido actualizado correctamente"    
+            lanzamensaje(res.status,res.statusText,"Actualización exitosa",mensajeaux,null)
+            break;
+            case 400:
+
+            break;
+            case 404:
+                let mensajeaux2= "El dato: "+params.state+"/"+params.year+" no se ha encontrado en la Base de datos"    
+                lanzamensaje(res.status,res.statusText,"Se ha producido un error al Actualizar",mensajeaux2,true)
+                
+            break;
+            case 409:
+            let mensajeaux3= "El dato: "+params.state+"/"+params.year+" ha solicitado actualizar el Estado o Año. Se ha denegado la actualización para velar por la integridad de la información"    
+                lanzamensaje(res.status,res.statusText,"Se ha producido un error al Actualizar",mensajeaux3,true)
+            break;
+            default:
+            lanzamensaje(res.status,res.statusText,"Se ha producido un error al buscar el elemento","Vaya... Algo ha salido mal. Probablemente la Base de Datos haya tenido un problema. Vuelva a intentarlo mas adelante",true)
+       
+            break;
+
+
+            }
 		});	
+
+        
+
+       
     
     };
 
@@ -100,8 +146,39 @@
     const toggleactualizar = () => (popactualizar = !popactualizar);
     const actualiza = () => {updateCrime(); popactualizar = !popactualizar};
  
-    
-    
+   
+
+
+    function gomain() {
+    location.href = '#/attacks';
+}
+
+  let botontomain=false;
+
+
+    //Modal alerta
+    let rescodigo=0;
+    let mensaje= "";
+    let resstatus="";
+    let mensajeespecifico="";
+    let error=false;
+
+    let alerta=false;
+    const lanzamensaje=(rc,rs,m,me,err)=>{
+
+    rescodigo=rc;
+    resstatus=rs;
+    mensaje=m;
+    mensajeespecifico=me;
+    error=err;//booleano
+
+    alerta=true;
+    }
+    const togglealerta=()=>{
+        alerta=!alerta;
+    }
+
+
  </script>
    
  
@@ -110,7 +187,7 @@
       <Table bordered  style="background-color: #F5EEF8 ; width:75% ; text-align: center; ">
          <thead style="background-color: #E8DAEF; color:black">
              <tr>
-                <td><b>Estado</b></td>
+                 <td><b>Estado</b></td>
                	<td><b>Año</b></td>
               	<td><b>Sexo masculino</b></td>
               	<td><b>Sexo femenino</b></td>
@@ -158,23 +235,44 @@
         </ModalFooter>
     </Modal>
  
-    <Modal isOpen={noencontrado} toggle={toggleactualizar} transitionOptions>
-        <ModalHeader {toggleactualizar}>Lo sentimos, no se ha encontrado el dato buscado</ModalHeader>
-        <ModalFooter>
+  
+   <Modal isOpen={alerta} toggle={togglealerta} transitionOptions>
+    <ModalHeader toggle={togglealerta} style="text-align: center;">{mensaje}
+    
+        
+    </ModalHeader>
+    <ModalBody style="text-align: center;">
+        {#if error!=null}
+            {#if error}
+            Tras realizar la operación hemos obtenido un codigo de error:
+            <p></p>
+            <a href="https://docs.google.com/presentation/d/1i79Yihxsynbjtar05xFXLXHChqEbsO44oaxg8mXWL6g/edit#slide=id.g10ecd5ec32_1_14"> 
+                {rescodigo} ({resstatus}).
+            </a>
+            <p>Causa:</p>
+             
+            <p>{mensajeespecifico}</p>
             
-            <Button color="secondary" on:click={pop}>Volver</Button>
-        </ModalFooter>
-    </Modal>
- 
+            {/if}
+        {:else}
+        <p>{mensajeespecifico}</p>
+        {/if}
+
+        <div>
+            <p></p>
+        <Button color="secondary" on:click={togglealerta}>Volver</Button>
+        {#if botontomain}
+        <Button color="secondary" on:click={gomain}>Volver a la tabla</Button>
+        {/if}
+    </div>
+    </ModalBody>
+    
+</Modal>
  
  
     </main>
  
  
  <style>
-    
-     
-    
- 
-     
+      
  </style>
