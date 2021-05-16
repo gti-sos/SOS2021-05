c0c951703bd411579bd662769856732c7d057439
manu261998 <manu261998@gmail.com>
2021-05-16 23:57:05 +0200

    Grafiaca comun flama

--- src/front/commongraph/statsGraph.svelte
+++ src/front/commongraph/statsGraph.svelte
@@ -39,13 +39,14 @@ let state= "Alabama"
     let data = [];
     let arraysales = [];
     let arrayattacks = [];
+    let arrayhomicides = [];
     onMount(buscar)
 
     async function getDataSales(state){
         arraysales = [];
         console.log("Fetching data...");
         const res = await fetch("/api/v2/arms-sales-stats?state="+state);
-        if(res.status==200){
+        if(res.status==200 ){
             console.log("Ok.");
             const json = await res.json();
             data = json;
@@ -57,6 +58,8 @@ let state= "Alabama"
             arraysales=arraysales.reverse()
             console.log(arraysales);
         }else{
+            "El estado: "+state+" no se ha encontrado en la Base de datos"    
+            lanzamensaje(res.status,res.statusText,"Se ha producido un error al cargar los datoso",mensajeaux2,true)
             console.log("Error!");
         }
     }
@@ -77,16 +80,35 @@ let state= "Alabama"
             arrayattacks= arrayattacks.reverse()
             console.log(arrayattacks );
         }else{
+            "El estado: "+state+" no se ha encontrado en la Base de datos"    
+            lanzamensaje(res.status,res.statusText,"Se ha producido un error al cargar los datoso",mensajeaux2,true)
             console.log("Error!");
         }
     }
-   
-  
-
-    function getVisibilidad(n) {
-        
-        return true
+    
+    async function getDataHomicides(state){
+        arrayhomicides = [];
+        console.log("Fetching data...");
+        const res = await fetch("/api/v2/homicides-by-firearms?state="+state);
+        if(res.ok){
+            console.log("Ok.");
+            const json = await res.json();
+            data = json;
+            console.log(`We have received ${data.length} data points.`);
+            for(let i=0;i<data.length;i++){
+                let aux= data[i].homicide_by_firearm.replace(".","")
+                arrayhomicides [i]=parseInt(aux,10)
+            }
+            arrayhomicides= arrayhomicides.reverse()
+            console.log(arrayhomicides );
+        }else{
+             "El estado: "+state+" no se ha encontrado en la Base de datos"    
+            lanzamensaje(res.status,res.statusText,"Se ha producido un error al cargar los datoso",mensajeaux2,true)
+            console.log("Error!");
+        }
     }
+    
+
 
    function loadGraph(){  
     
@@ -96,6 +118,8 @@ let state= "Alabama"
     i = 0;
    
     let arrayporagno=[]
+    let arrayhomicidesarms=[]
+    let arrayhomicidesattacks=[]
     for(i;i<len-1;i++){
         let j=0
        
@@ -111,16 +135,30 @@ let state= "Alabama"
             
              console.log(a)
              arrayporagno.push(arrayattacks[i]/a)
+             arrayhomicidesarms.push(arrayhomicides[i]/a)
+             arrayhomicidesattacks.push(arrayhomicides[i]/arrayattacks[i])
          
     }
     
     seriesaux.push({
             name: "Ataques/Armas Vendidas",
             data:  arrayporagno,
-            visible: getVisibilidad(state)
+            visible: true
               
          });
-    
+         seriesaux.push({
+            name: "Homicidios/Armas Vendidas",
+            data:  arrayhomicidesarms,
+            visible: true
+              
+         });
+
+         seriesaux.push({
+            name: "Homicidios/Ataques",
+            data:  arrayhomicidesattacks,
+            visible: false
+              
+         });
     
 
     //SERIES PARA LOS ESTADOS
@@ -176,11 +214,20 @@ const busqueda=()=>{
 }
 async function buscar(){
     b=!b;
-   console.log(state)
+    if(estados.includes(state)){
+
+        console.log(state)
    getDataSales(state)
    getDataAttacks(state)
+   getDataHomicides(state)
    await delay(500);
    recarga()
+    }else{
+        
+            lanzamensaje(0,"h","Se ha producido un error al cargar los datos","El estado: "+state+" no corresponde con ningun estado de EEUU"    ,null)
+            
+    }
+   
    
   
    
@@ -191,6 +238,27 @@ const recarga=()=>{
    
 }
  
+let rescodigo=0;
+let mensaje= "";
+let resstatus="";
+let mensajeespecifico="";
+let error=false;
+let alerta=false;
+const lanzamensaje=(rc,rs,m,me,err)=>{
+
+    rescodigo=rc;
+    resstatus=rs;
+    mensaje=m;
+    mensajeespecifico=me;
+    error=err;//booleano
+
+    alerta=true;
+}
+const togglealerta=()=>{
+    alerta=!alerta;
+}
+
+
 const delay = ms => new Promise(res => setTimeout(res, ms));
 </script>
 
@@ -226,6 +294,37 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
             <Button color="primary" on:click={buscar}>Vamos allá!</Button>
             <Button color="secondary" on:click={busqueda}>Cancelar</Button>
         </ModalFooter>
+    </Modal>
+
+
+    <Modal isOpen={alerta} toggle={togglealerta} transitionOptions>
+        <ModalHeader toggle={togglealerta} style="text-align: center;">{mensaje}
+        
+            
+        </ModalHeader>
+        <ModalBody style="text-align: center;">
+            {#if error!=null}
+                {#if error}
+                Tras realizar la operación hemos obtenido un codigo de error:
+                <p></p>
+                <a href="https://docs.google.com/presentation/d/1i79Yihxsynbjtar05xFXLXHChqEbsO44oaxg8mXWL6g/edit#slide=id.g10ecd5ec32_1_14"> 
+                    {rescodigo} ({resstatus}).
+                </a>
+                <p>Causa posible:</p>
+                 
+                <p>{mensajeespecifico}</p>
+                
+                {/if}
+            {:else}
+            <p>{mensajeespecifico}</p>
+            {/if}
+
+            <div>
+                <p></p>
+            <Button color="secondary" on:click={togglealerta}>Volver</Button>
+        </div>
+        </ModalBody>
+        
     </Modal>
 </main>
 <style>
