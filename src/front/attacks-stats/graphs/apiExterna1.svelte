4ecb029ae6a146433113487e6885f2f7260e14d3
josanmegias <josepantoniomegias@hotmail.com>
2021-05-27 20:45:34 +0200

    External 1

---
+++ src/front/attacks-stats/graphs/apiExterna1.svelte
@@ -0,0 +1,99 @@
+<script>
+
+    async function loadGraph(){
+
+    Highcharts.chart('container', {
+    title: {
+        text: 'Chart.update'
+    },
+    subtitle: {
+        text: 'Plain'
+    },
+    xAxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    },
+    series: [{
+        type: 'column',
+        colorByPoint: true,
+        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+        showInLegend: false
+    }]
+});
+
+document.getElementById('plain').addEventListener('click', () => {
+    chart.update({
+        chart: {
+            inverted: false,
+            polar: false
+        },
+        subtitle: {
+            text: 'Plain'
+        }
+    });
+});
+
+document.getElementById('inverted').addEventListener('click', () => {
+    chart.update({
+        chart: {
+            inverted: true,
+            polar: false
+        },
+        subtitle: {
+            text: 'Inverted'
+        }
+    });
+});
+
+document.getElementById('polar').addEventListener('click', () => {
+    chart.update({
+        chart: {
+            inverted: false,
+            polar: true
+        },
+        subtitle: {
+            text: 'Polar'
+        }
+    });
+});
+
+
+    }
+
+</script>
+
+<svelte:head>
+
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/highcharts-more.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
+    <script src="https://code.highcharts.com/modules/export-data.js"></script>
+    <script src="https://code.highcharts.com/modules/accessibility.js" on:load="{loadGraph}"></script>
+
+</svelte:head>
+
+
+<main>
+
+    <figure class="highcharts-figure">
+        <div id="container"></div>
+        <p class="highcharts-description">
+            Chart with buttons to modify options, showing how options can be changed
+            on the fly. This flexibility allows for more dynamic charts.
+        </p>
+    
+        <button id="plain">Plain</button>
+        <button id="inverted">Inverted</button>
+        <button id="polar">Polar</button>
+    </figure>
+
+</main>
+
+<style>
+
+#container {
+    height: 400px; 
+}
+
+
+
+</style>
