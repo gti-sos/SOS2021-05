cd0049c5a40d8edc86d96ef2df7da1fe1adc21e0
josanmegias <josepantoniomegias@hotmail.com>
2021-06-07 00:12:11 +0200

    e2e modigicado

--- test/e2eJosan.js
+++ test/e2eJosan.js
@@ -10,15 +10,15 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
   const page = await browser.newPage();
 
     //CAPTURA INDEX
-  await page.goto("http://sos2021-05.herokuapp.com/#/info");
+  await page.goto("https://sos2021-05.herokuapp.com/#/info");
   await delay(1000)
-  await page.screenshot({ path: './test/capturas/JoseAntonio/index.png'});
+  await page.screenshot({ path: './test/capturas/JoseAntonio/index.png' });
   console.log("Index abierto");
   
 //////////// CAPTURAS AL FRONT END TABLA
-  await page.goto("http://sos2021-05.herokuapp.com/#/attacks");
+  await page.goto("https://sos2021-05.herokuapp.com/#/attacks");
   await delay(1000)
-  await page.screenshot({ path: './test/capturas/JoseAntonio/frontAttacks.png'});
+  await page.screenshot({ path: './test/capturas/JoseAntonio/frontAttacks.png' });
   console.log("Front End abierto");
 
 ///////////////////CAPTURA DELETE DATO
@@ -26,12 +26,12 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
     page.click("body > main > main > table > tbody > tr:nth-child(1) > td:nth-child(12) > button:nth-child(1)")
   ]);
   await delay(2000);
-  await page.screenshot({ path: './test/capturas/JoseAntonio/frontAttacksDelete.png'});
+  await page.screenshot({ path: './test/capturas/JoseAntonio/frontAttacksDelete.png' });
   console.log("Dato borrado");
 
 
 //Para 'volver'
-await page.goto("http://sos2021-05.herokuapp.com/#/attacks");
+await page.goto("https://sos2021-05.herokuapp.com/#/attacks");
 await delay(2000);
 
 //para insertar
@@ -41,7 +41,7 @@ const [response1] = await Promise.all([
 ]);
 
 await delay(2000);
-await page.screenshot({ path: './test/capturas/JoseAntonio/frontAttacksInsert.png'});
+await page.screenshot({ path: './test/capturas/JoseAntonio/frontAttacksInsert.png' });
 console.log("Insert");
 
 
@@ -58,7 +58,7 @@ const [response3] = await Promise.all([
 ]);
 
 await delay(500);
-await page.screenshot({ path: './test/capturas/JoseAntonio/frontAttacksUpdate.png'})
+await page.screenshot({ path: './test/capturas/JoseAntonio/frontAttacksUpdate.png' })
 console.log("Captura Update");
 
 const [response4] = await Promise.all([
@@ -73,7 +73,7 @@ const [response4] = await Promise.all([
 console.log("Click en actualizar");
 
 await delay(2000)
-await page.screenshot({ path: './test/capturas/JoseAntonio/frontAttacksUpdatePop.png'})
+await page.screenshot({ path: './test/capturas/JoseAntonio/frontAttacksUpdatePop.png' })
 
 
 const [response6] = await Promise.all([
@@ -82,7 +82,7 @@ const [response6] = await Promise.all([
 console.log("Click en actualizar por segunda vez");
 
 await delay(2000)
-await page.screenshot({ path: './test/capturas/JoseAntonio/frontAttacksUpdateOK.png'})
+await page.screenshot({ path: './test/capturas/JoseAntonio/frontAttacksUpdateOK.png' })
 
 
 const [response7] = await Promise.all([
@@ -91,15 +91,23 @@ const [response7] = await Promise.all([
   console.log("Click en volver a la tabla");
   
   await delay(2000)
-  await page.screenshot({ path: './test/capturas/JoseAntonio/frontAttacksTablaActualizada.png'})
+  await page.screenshot({ path: './test/capturas/JoseAntonio/frontAttacksTablaActualizada.png' })
   console.log("No se nota el cambio de los datos actualizados porque son los mismos");
 
 
 
 //Para hacer captura de mi index de gráficas
 await page.goto("http://sos2021-05.herokuapp.com/#/integrations");
+
+const [response8] = await Promise.all([
+  page.click("body > main > main > div:nth-child(4) > button:nth-child(3)"),
+]);
+console.log("Click en Ataques-Jose Antonio Megias");
+
 await delay(2000);
-await page.screenshot({ path: './test/capturas/JoseAntonio/indexGraficasAttacks.png'});
+await page.screenshot({ path: './test/capturas/JoseAntonio/indexGraficasAttacks.png' });
+
+
 
 //vamos a una de mis gráficas y captura
 await page.goto('http://sos2021-05.herokuapp.com/#/attacks/apiexterna2');
