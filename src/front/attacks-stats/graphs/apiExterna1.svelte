7b0c2467159ba01d0bd545a294dc399a46ba2d09
josanmegias <josepantoniomegias@hotmail.com>
2021-06-06 23:49:32 +0200

    Grafica Uso1 externa cambiada

--- src/front/attacks-stats/graphs/apiExterna1.svelte
+++ src/front/attacks-stats/graphs/apiExterna1.svelte
@@ -23,8 +23,8 @@
 async function inicio(){
    
 	await getData()
-    delay(29);
-   recarga()
+    await delay(29);
+    recarga()
    
 }
 const recarga=()=>{
@@ -67,23 +67,32 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 
     async function loadGraph(){
 
-    Highcharts.chart('container', {
+        Highcharts.chart('container', {
+    chart: {
+        type: 'cylinder',
+        options3d: {
+            enabled: true,
+            alpha: 15,
+            beta: 15,
+            depth: 50,
+            viewDistance: 25
+        }
+    },
     title: {
         text: 'Capacidad de los circuitos de la Formula 1'
     },
-    
+    plotOptions: {
+        series: {
+            depth: 30,
+            colorByPoint: true
+        }
+    },
     xAxis: {
         categories: circuitos // aqui meter los nombres de los circuitos
     },
-    yAxis: {
-        title: {
-            text: 'Capacidad'
-    }
-    },
     series: [{
-        type: 'column',
-        colorByPoint: true,
-        data: capacidades, // AQUI METER LAS CAPACIDADES DE LOS CIRCUITOS
+        data: capacidades,
+        name: 'Capacidad',
         showInLegend: false
     }]
 });
@@ -103,7 +112,9 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 
 <main>
 
-    <script src="https://code.highcharts.com/highcharts.js" on:load="{loadGraph}"></script>
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/highcharts-3d.js"></script>
+    <script src="https://code.highcharts.com/modules/cylinder.js" on:load="{loadGraph}"></script>
 
     <div>   <Button outline style=" background-color:#6C00AF; color:aliceblue" on:click="{pop}"> Volver</Button></div> 
 
@@ -117,7 +128,41 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 <style>
 
 #container {
-    height: 400px; 
+    height: 600px; 
+}
+
+.highcharts-figure, .highcharts-data-table table {
+    min-width: 400px; 
+    max-width: 800px;
+    margin: 1em auto;
+}
+
+.highcharts-data-table table {
+    font-family: Verdana, sans-serif;
+    border-collapse: collapse;
+    border: 1px solid #EBEBEB;
+    margin: 10px auto;
+    text-align: center;
+    width: 100%;
+    max-width: 500px;
+}
+.highcharts-data-table caption {
+    padding: 1em 0;
+    font-size: 1.2em;
+    color: #555;
+}
+.highcharts-data-table th {
+	font-weight: 600;
+    padding: 0.5em;
+}
+.highcharts-data-table td, .highcharts-data-table th, .highcharts-data-table caption {
+    padding: 0.5em;
+}
+.highcharts-data-table thead tr, .highcharts-data-table tr:nth-child(even) {
+    background: #f8f8f8;
+}
+.highcharts-data-table tr:hover {
+    background: #f1f7ff;
 }
 
 
