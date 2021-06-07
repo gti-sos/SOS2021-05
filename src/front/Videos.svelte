93597d7961a12637d8018e9e83626f9a2d279dc2
josanmegias <josepantoniomegias@hotmail.com>
2021-06-07 14:50:25 +0200

    Video

--- src/front/Videos.svelte
+++ src/front/Videos.svelte
@@ -67,7 +67,9 @@
           </CardHeader>
           <CardBody>
             <CardSubtitle />
-            Video
+            
+            <iframe width="560" height="315" src="https://www.youtube.com/watch?v=H5AxAk4Ao8Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
 
           </CardBody>
           <CardFooter
