ec65c30758c87d49e33b59850df1a5f1904f7a1c
manu261998 <manu261998@gmail.com>
2021-06-07 04:30:31 +0200

    VIDEPO MANUEL

--- src/front/Videos.svelte
+++ src/front/Videos.svelte
@@ -50,6 +50,7 @@
           </CardHeader>
           <CardBody>
             <CardSubtitle />
+            
             <iframe width="560" height="315" src="https://www.youtube.com/embed/avvu61tny7I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
           </CardBody>
