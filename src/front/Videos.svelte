880d9e0a6dce718ce921e57b8bbb9dad1672faff
josanmegias <josepantoniomegias@hotmail.com>
2021-06-07 15:45:20 +0200

    Video

--- src/front/Videos.svelte
+++ src/front/Videos.svelte
@@ -68,7 +68,7 @@
           <CardBody>
             <CardSubtitle />
             
-            <iframe width="560" height="315" src="https://youtu.be/H5AxAk4Ao8Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/H5AxAk4Ao8Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
 
           </CardBody>
